f51e7c484d13d4eb63fc1beec8ddb4c6c56b6b7c
Silviu Stroe <silviu@silviu-s.com>
2023-12-26 16:20:54 +0200

    Improve UX, add warm-up before testing, update disclaimer

--- script.js
+++ script.js
@@ -21,6 +21,39 @@ const checkButton = document.getElementById('checkButton');
 const editButton = document.getElementById('editButton');
 const topWebsites = ['google.com', 'youtube.com', 'facebook.com', 'amazon.com', 'yahoo.com', 'wikipedia.org', 'twitter.com', 'instagram.com', 'linkedin.com', 'netflix.com'];
 // Global variable to store chart instance
+const dnsServers = [{name: "AdGuard", url: "https://dns.adguard-dns.com/dns-query"}, {
+    name: "AliDNS", url: "https://dns.alidns.com/dns-query"
+}, //{ name: "Bebasid", url: "https://dns.bebasid.com/dns-query", type: "get" },
+    {
+        name: "OpenDNS", url: "https://doh.opendns.com/dns-query"
+    }, {name: "Cloudflare", url: "https://cloudflare-dns.com/dns-query", type: "get", allowCors: true}, {
+        name: "ControlD", url: "https://freedns.controld.com/p0"
+    }, //{ name: "Decloudus", url: "https://dns.decloudus.com/dns-query" , type: "get" },
+    {name: "DNS.SB", url: "https://doh.dns.sb/dns-query", type: "get", allowCors: true}, {
+        name: "DNSPod", url: "https://dns.pub/dns-query", type: "get", allowCors: true
+    }, {name: "Google", url: "https://dns.google/resolve", type: "get", allowCors: true}, {
+        name: "Mullvad", url: "https://doh.mullvad.net/dns-query"
+    }, {
+        name: "NextDNS", url: "https://dns.nextdns.io", type: "get"
+    }, {name: "OpenBLD", url: "https://ada.openbld.net/dns-query"}, {
+        name: "DNS0.EU", url: "https://zero.dns0.eu/"
+    }, {name: "Quad9", url: "https://dns.quad9.net/dns-query"}, {
+        name: "360", url: "https://doh.360.cn/dns-query"
+    }, //{ name: "Applied Privacy", url: "https://doh.applied-privacy.net/query" , type: "get" },
+    {
+        name: "Canadian Shield", url: "https://private.canadianshield.cira.ca/dns-query"
+    }, //{ name: "Comss.one", url: "https://dns.comss.one/dns-query", type: "get"  },
+    {
+        name: "Digitale Gesellschaft", url: "https://dns.digitale-gesellschaft.ch/dns-query"
+    }, {
+        name: "DNS for Family", url: "https://dns-doh.dnsforfamily.com/dns-query"
+    }, {name: "Restena", url: "https://kaitain.restena.lu/dns-query"}, {
+        name: "IIJ", url: "https://public.dns.iij.jp/dns-query"
+    }, {
+        name: "LibreDNS", url: "https://doh.libredns.gr/dns-query"
+    }, //{ name: "RoTunneling DNS", url: "https://dns.rotunneling.net/dns-query/public" , type: "get" },
+    {name: "Switch", url: "https://dns.switch.ch/dns-query"}];
+
 let dnsChart;
 
 function updateChartWithData(server) {
@@ -40,10 +73,8 @@ function updateChartWithData(server) {
     // Initialize the chart if it doesn't exist
     if (!dnsChart) {
         dnsChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: [],
-                datasets: [{
+            type: 'bar', data: {
+                labels: [], datasets: [{
                     label: 'Min Speed (ms)',
                     data: [],
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
@@ -62,10 +93,8 @@ function updateChartWithData(server) {
                     borderColor: 'rgba(255, 206, 86, 1)',
                     borderWidth: 1
                 }]
-            },
-            options: {
-                responsive: true,
-                scales: {
+            }, options: {
+                responsive: true, scales: {
                     y: {
                         beginAtZero: true
                     }
@@ -93,11 +122,24 @@ function updateChartWithData(server) {
     dnsChart.update();
 }
 
+async function warmUpDNSServers() {
+    // Display the warm-up message
+    // Use the same DNS server list and top websites for warm-up
+    for (const server of dnsServers) {
+        // Perform DNS queries for each website in the topWebsites list
+        await Promise.all(topWebsites.map(website => measureDNSSpeed(server.url, website, server.type, server.allowCors)));
+    }
+    console.log("Warm-up phase completed");
+}
+
 checkButton.addEventListener('click', async function () {
     this.disabled = true;
     editButton.disabled = true; // Disable the Edit button
     document.getElementById('loadingMessage').style.display = 'block';
 
+    document.getElementById('loadingMessage').innerHTML = 'Warming up DNS servers <img height="70" width="70" src="loading.gif"  alt="Loading" style="display: inline; vertical-align: middle;" />';
+    await warmUpDNSServers();
+    document.getElementById('loadingMessage').innerHTML = 'Analyzing DNS servers <img height="70" width="70" src="loading.gif" alt="Loading" style="display: inline; vertical-align: middle;" />';
     await performDNSTests();
 
     document.getElementById('loadingMessage').style.display = 'none';
@@ -106,45 +148,12 @@ checkButton.addEventListener('click', async function () {
 });
 
 async function performDNSTests() {
-    const dnsServers = [{name: "AdGuard", url: "https://dns.adguard-dns.com/dns-query"}, {
-        name: "AliDNS", url: "https://dns.alidns.com/dns-query"
-    }, //{ name: "Bebasid", url: "https://dns.bebasid.com/dns-query", type: "get" },
-        {
-            name: "OpenDNS", url: "https://doh.opendns.com/dns-query"
-        }, {name: "Cloudflare", url: "https://cloudflare-dns.com/dns-query", type: "get", allowCors: true}, {
-            name: "ControlD", url: "https://freedns.controld.com/p0"
-        }, //{ name: "Decloudus", url: "https://dns.decloudus.com/dns-query" , type: "get" },
-        {name: "DNS.SB", url: "https://doh.dns.sb/dns-query", type: "get", allowCors: true},
-        {name: "DNSPod", url: "https://dns.pub/dns-query", type: "get", allowCors: true},
-        {name: "Google", url: "https://dns.google/resolve", type: "get", allowCors: true}, {
-            name: "Mullvad", url: "https://doh.mullvad.net/dns-query"
-        }, {
-            name: "NextDNS", url: "https://dns.nextdns.io", type: "get"
-        }, {name: "OpenBLD", url: "https://ada.openbld.net/dns-query"}, {
-            name: "DNS0.EU", url: "https://zero.dns0.eu/"
-        }, {name: "Quad9", url: "https://dns.quad9.net/dns-query"}, {
-            name: "360", url: "https://doh.360.cn/dns-query"
-        }, //{ name: "Applied Privacy", url: "https://doh.applied-privacy.net/query" , type: "get" },
-        {
-            name: "Canadian Shield", url: "https://private.canadianshield.cira.ca/dns-query"
-        }, //{ name: "Comss.one", url: "https://dns.comss.one/dns-query", type: "get"  },
-        {
-            name: "Digitale Gesellschaft", url: "https://dns.digitale-gesellschaft.ch/dns-query"
-        }, {
-            name: "DNS for Family", url: "https://dns-doh.dnsforfamily.com/dns-query"
-        }, {name: "Restena", url: "https://kaitain.restena.lu/dns-query"}, {
-            name: "IIJ", url: "https://public.dns.iij.jp/dns-query"
-        }, {
-            name: "LibreDNS", url: "https://doh.libredns.gr/dns-query"
-        }, //{ name: "RoTunneling DNS", url: "https://dns.rotunneling.net/dns-query/public" , type: "get" },
-        {name: "Switch", url: "https://dns.switch.ch/dns-query"}];
-
 
     for (const server of dnsServers) {
         const speedResults = await Promise.all(topWebsites.map(website => measureDNSSpeed(server.url, website, server.type, server.allowCors)));
 
         // Store individual results for detailed view
-        server.individualResults = speedResults.map((speed, index) => ({ website: topWebsites[index], speed }));
+        server.individualResults = speedResults.map((speed, index) => ({website: topWebsites[index], speed}));
 
         const validResults = speedResults.filter(speed => speed !== null && typeof speed === 'number');
         validResults.sort((a, b) => a - b);
@@ -309,7 +318,9 @@ function sortTable(columnIndex) {
         let cellB = b[0].getElementsByTagName("TD")[columnIndex];
 
         if (!cellA || !cellB) {
-            console.error("Undefined cell encountered", { cellA, cellB, rowIndexA: a[0].rowIndex, rowIndexB: b[0].rowIndex });
+            console.error("Undefined cell encountered", {
+                cellA, cellB, rowIndexA: a[0].rowIndex, rowIndexB: b[0].rowIndex
+            });
             return 0;
         }
 
@@ -352,7 +363,7 @@ document.getElementById('cta').addEventListener('click', function () {
 });
 
 // Add this at the top of your script, after the global variable declarations
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     updateChartVisibility();
     if (dnsChart) {
         dnsChart.resize(); // This line triggers the chart to resize
@@ -375,9 +386,9 @@ function updateChartVisibility() {
 }
 
 // JavaScript to handle modal and list manipulation
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     updateChartVisibility();
-    document.getElementById('resultsTable').addEventListener('click', function(event) {
+    document.getElementById('resultsTable').addEventListener('click', function (event) {
         let row = event.target.closest('tr');
         if (row && !row.classList.contains('details-row')) {
             let detailsRow = row.nextElementSibling;
@@ -402,7 +413,7 @@ document.addEventListener('DOMContentLoaded', function() {
             li.textContent = site;
             const removeBtn = document.createElement("button");
             removeBtn.textContent = '-';
-            removeBtn.onclick = function() {
+            removeBtn.onclick = function () {
                 topWebsites.splice(index, 1);
                 renderList();
             };
@@ -414,7 +425,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Open the modal
-    btn.onclick = function() {
+    btn.onclick = function () {
         modal.style.display = "block";
         renderList();
     }
@@ -439,12 +450,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Close the modal
-    span.onclick = function() {
+    span.onclick = function () {
         modal.style.display = "none";
     }
 
     // Add new website
-    addBtn.onclick = function() {
+    addBtn.onclick = function () {
         const host = validateAndExtractHost(input.value);
         if (host) {
             if (!topWebsites.includes(host)) {
@@ -460,8 +471,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Close the modal when clicking outside of it
-    window.onclick = function(event) {
-        if (event.target == modal) {
+    window.onclick = function (event) {
+        if (event.target === modal) {
             modal.style.display = "none";
         }
     }
